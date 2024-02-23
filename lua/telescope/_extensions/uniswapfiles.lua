dfabbdf9c1a6cb8c4caac903d7cfa010bfe042f6
Erik Lilja <erikvlilja@gmail.com>
2024-02-23 08:25:28 +0100

    rename file

--- lua/telescope/_extensions/uniswapfiles.lua
+++ lua/telescope/_extensions/uniswapfiles.lua
@@ -1,6 +1,7 @@
 return require("telescope").register_extension {
   setup = function(ext_config, config)
     -- access extension config and user config
+    print("Registered telescope_swap_files with config")
   end,
   exports = {
     telescope_swap_files = require("telescope_swap_files")()
