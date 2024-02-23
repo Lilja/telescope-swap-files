5688f37d4220e4110a3067469c546f61b4264771
Erik Lilja <erikvlilja@gmail.com>
2024-02-23 13:43:02 +0100

    less printing

--- lua/telescope/_extensions/uniswapfiles.lua
+++ lua/telescope/_extensions/uniswapfiles.lua
@@ -1,10 +1,4 @@
-print("Loading telescope_swap_files")
-
 return require("telescope").register_extension({
-	setup = function(ext_config, config)
-		-- access extension config and user config
-		print("Registered telescope_swap_files with config")
-	end,
 	exports = {
 		telescope_swap_files = require("telescope_swap_files"),
 	},
